4048c19f7d34434d5e7bc494b0cb7c7b40b68302
ardiyantop8 <ardiyantop8@gmail.com>
2022-03-21 11:35:46 +0700

    manipulasi array key

---
+++ manipulasi_array_key.php
@@ -0,0 +1,17 @@
+<?php
+
+/* inisiasi array */
+$list_array     = array();
+$list_array2    = array();
+$array_buah     = array('pisang', 'jeruk', 'apel');
+$array_warna    = array('kuning','orange','merah'); 
+
+/*mengisi $list_array dari satu variable array buah*/
+foreach ($array_buah as $key => $value) {
+    $list_array[$value] = $value;
+}
+// echo "<pre>";print_r($list_array);die;
+/*END mengisi $list_array dari satu variable array buah*/
+
+
+?>
